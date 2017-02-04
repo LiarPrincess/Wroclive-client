de4fc8c0375d9cb0d9ec11c54c461b6f99e9840e
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2017-02-04 22:35:16 +0100

    Proper project structure

---
+++ Application/Delegates/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  Created by NoPoint
+//  Copyright © 2017 NoPoint. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+}
---
+++ Frontend/MainController.swift
@@ -0,0 +1,10 @@
+//
+//  Created by NoPoint
+//  Copyright © 2017 NoPoint. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+}
