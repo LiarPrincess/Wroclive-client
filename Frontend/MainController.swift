2ffe74df2a131101bdf05dd0036e8127a1323c2a
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2017-02-05 14:01:45 +0100

    [Main] Added button images

--- Frontend/MainController.swift
+++ Frontend/MainController.swift
@@ -9,7 +9,15 @@ class MainController: UIViewController {
 
   //MARK: Properties
 
+  @IBOutlet weak var buttonCurrentLocation: UIButton!
+  @IBOutlet weak var buttonSearch: UIButton!
+  @IBOutlet weak var buttonFavorites: UIButton!
+  @IBOutlet weak var buttonSettings: UIButton!
   @IBOutlet weak var buttonContainer: UIVisualEffectView!
+
+  lazy var allButtons: [UIButton] = {
+    return [self.buttonCurrentLocation, self.buttonSearch, self.buttonFavorites, self.buttonSettings]
+  }()
 
   //MARK: Overriden
 
@@ -31,7 +39,13 @@ extension MainController {
   func applyVisualStyles() {
 
     //[buttonContainer] top border
-    self.buttonContainer.addBorder(edges: .top, color: UIColor(white: 0.75, alpha: 0.5), thickness: 1.0)
+    self.buttonContainer.addBorder(edges: .top, color: UIColor(white: 0.80, alpha: 0.65), thickness: 0.75)
+
+    //[buttons] center and scale image
+    for button in self.allButtons {
+      button.imageEdgeInsets = UIEdgeInsets(top: 10.0, left: 0.0, bottom: 10.0, right: 0.0)
+      button.imageView?.contentMode = .scaleAspectFit
+    }
   }
 
 }
