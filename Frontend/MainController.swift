9f6735a34476b2310755c6b04ecfc081237e55b4
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2017-02-05 01:22:56 +0100

    [MainController] Added bottom buttons

--- Frontend/MainController.swift
+++ Frontend/MainController.swift
@@ -7,4 +7,31 @@ import UIKit
 
 class MainController: UIViewController {
 
+  //MARK: Properties
+
+  @IBOutlet weak var buttonContainer: UIVisualEffectView!
+
+  //MARK: Overriden
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    self.applyVisualStyles()
+  }
+
+  //MARK: Gesture recognizers
+  
+  
+  //MARK: Methods
+
+}
+
+extension MainController {
+
+  func applyVisualStyles() {
+
+    //[buttonContainer] top border
+    self.buttonContainer.addBorder(edges: .top, color: UIColor(white: 0.75, alpha: 0.5), thickness: 1.0)
+  }
+
 }
