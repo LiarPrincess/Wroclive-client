13c8808741a3a0bf0e08b530e3b0a10880f5e387
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2017-07-10 22:30:02 +0200

    [Managers] yolo managers

--- Sources/Modules/Functional/Managers.swift
+++ Sources/Modules/Functional/Managers.swift
@@ -4,61 +4,8 @@
 //
 
 struct Managers {
-
-  // MARK: - Bookmark
-
-  private static var _bookmark: BookmarksManager?
-
-  static var bookmark: BookmarksManager {
-    get {
-      guard let bookmark = _bookmark else {
-        fatalError("Bookmark manager has not been registered!")
-      }
-      return bookmark
-    }
-    set { _bookmark = newValue }
-  }
-
-  // MARK: - Lines
-
-  private static var _lines: LinesManager?
-
-  static var lines: LinesManager {
-    get {
-      guard let lines = _lines else {
-        fatalError("Lines manager has not been registered!")
-      }
-      return lines
-    }
-    set { _lines = newValue }
-  }
-
-  // MARK: - Search state
-
-  private static var _searchState: SearchStateManager?
-
-  static var searchState: SearchStateManager {
-    get {
-      guard let searchState = _searchState else {
-        fatalError("Search state manager has not been registered!")
-      }
-      return searchState
-    }
-    set { _searchState = newValue }
-  }
-
-  // MARK: - Location
-
-  private static var _location: LocationManager?
-
-  static var location: LocationManager {
-    get {
-      guard let location = _location else {
-        fatalError("Location manager has not been registered!")
-      }
-      return location
-    }
-    set { _location = newValue }
-  }
-
+  static var bookmark:    BookmarksManager!
+  static var lines:       LinesManager!
+  static var searchState: SearchStateManager!
+  static var location:    LocationManager!
 }
