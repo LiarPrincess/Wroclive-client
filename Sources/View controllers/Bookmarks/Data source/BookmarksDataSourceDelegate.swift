3a73b1eec8602e950d147db362852915755ebfa2
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2017-12-26 20:08:38 +0100

    [Bookmarks] Removed 'Data sources'

--- Sources/View controllers/Bookmarks/Data source/BookmarksDataSourceDelegate.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Created by Michal Matuszczyk
-//  Copyright © 2017 Michal Matuszczyk. All rights reserved.
-//
-
-protocol BookmarksDataSourceDelegate: class {
-  func dataSource(_ dataSource: BookmarksDataSource, didDelete bookmark: Bookmark)
-  func didReorderBookmarks(_ dataSource: BookmarksDataSource)
-}
