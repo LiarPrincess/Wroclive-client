c376fcd65694ea2f830ee4c166ed48726d71b6b2
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2018-04-18 22:51:52 +0200

    [Environment] Simplified stack

--- Sources/Modules/AppEnvironment.swift
+++ Sources/Modules/AppEnvironment.swift
@@ -3,30 +3,16 @@
 //  Copyright © 2017 Michal Matuszczyk. All rights reserved.
 //
 
-typealias Managers = AppEnvironment
+// swiftlint:disable identifier_name
+var Managers: Environment {
+  return AppEnvironment.current
+}
 
 struct AppEnvironment {
 
-  // MARK: Managers
-
-  static var bundle:       BundleManagerType       { return current.bundle       }
-  static var device:       DeviceManagerType       { return current.device       }
-  static var network:      NetworkManagerType      { return current.network      }
-  static var userLocation: UserLocationManagerType { return current.userLocation }
-  static var documents:    DocumentsManagerType    { return current.documents    }
-  static var debug:        DebugManagerType        { return current.debug        }
-
-  static var theme:     ThemeManagerType     { return current.theme     }
-  static var api:       ApiManagerType       { return current.api       }
-  static var search:    SearchManagerType    { return current.search    }
-  static var bookmarks: BookmarksManagerType { return current.bookmarks }
-  static var map:       MapManagerType       { return current.map       }
-
-  // MARK: Stack
-
   private static var stack: [Environment] = []
 
-  private static var current: Environment {
+  static var current: Environment {
     precondition(stack.any, "Attempting to use empty environment stack.")
     return stack.last!
   }
