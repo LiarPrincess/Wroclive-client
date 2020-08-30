d517d5615aa968fb46cdcffe99c801d24a1a60f8
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2020-08-30 15:01:43 +0200

    [Card] Removed 'DismissGestureHandlerType'

--- WrocliveFramework/Helpers/Card panel/Dismiss gesture/DismissGestureHandler.swift
+++ WrocliveFramework/Helpers/Card panel/Dismiss gesture/DismissGestureHandler.swift
@@ -6,7 +6,7 @@ import UIKit
 
 private typealias Constants = CardPanelConstants.DismissGesture
 
-internal class DismissGestureHandler: DismissGestureHandlerType {
+internal class DismissGestureHandler {
 
   // MARK: - Properties
 
