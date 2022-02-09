c017a1fa27520edc3007c4a89fc2f0fe7b085efd
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2022-02-09 17:41:47 +0100

    [Notification] Added 'NotificationTokenSendLimiter'

---
+++ WrocliveFramework/Environment/Notifications/NotificationTokenSendLimiter.swift
@@ -0,0 +1,87 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import Foundation
+
+private let second = 1.0
+private let minute = 60 * second
+private let hour = 60 * minute
+
+public struct StoredNotificationToken: Equatable {
+
+  public let date: Date
+  public let value: String
+
+  public init(date: Date, value: String) {
+    self.date = date
+    self.value = value
+  }
+}
+
+public protocol NotificationTokenStore {
+  func getNotificationToken() -> StoredNotificationToken?
+  func setNotificationToken(token: StoredNotificationToken)
+}
+
+// MARK: - Send limiter
+
+/// We will send new token to server, but only once in a while.
+/// We don't want to use too much battery when user opens/closes the app repeatedly.
+///
+/// It also handles the edge case when we did send the token but it got lost.
+public protocol NotificationTokenSendLimiterType {
+  func shouldSend(token: String) -> Bool
+  func registerSend(token: String)
+}
+
+public final class NotificationTokenSendLimiter: NotificationTokenSendLimiterType {
+
+  public typealias GetDate = () -> Date
+
+  public static let sendInterval = 23 * hour
+
+  private let store: NotificationTokenStore
+  private let getDate: GetDate
+
+  public init(store: NotificationTokenStore, getDate: GetDate? = nil) {
+    self.store = store
+    self.getDate = getDate ?? Date.init
+  }
+
+  public func shouldSend(token: String) -> Bool {
+    // No token stored
+    guard let storedToken = self.store.getNotificationToken() else {
+      return true
+    }
+
+    let oldToken = storedToken.value
+    let hasTokenChanged = token != oldToken
+
+    if hasTokenChanged {
+      return true
+    }
+
+    // We will ignore all of the nuances of the time zones.
+    let storeDate = storedToken.date
+    let storeDate1970 = storeDate.timeIntervalSince1970
+
+    let now = self.getDate()
+    let now1970 = now.timeIntervalSince1970
+
+    // If we somehow stored the value in the future then we will assume app error.
+    let isStoredInFuture = storeDate1970 >= now1970
+    if isStoredInFuture {
+      return true
+    }
+
+    let interval = now1970 - storeDate1970
+    return interval >= NotificationTokenSendLimiter.sendInterval
+  }
+
+  public func registerSend(token: String) {
+    let now = self.getDate()
+    let storedToken = StoredNotificationToken(date: now, value: token)
+    self.store.setNotificationToken(token: storedToken)
+  }
+}
---
+++ WrocliveTests/Environment/Notifications/NotificationTokenSendLimiterTests.swift
@@ -0,0 +1,124 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import XCTest
+import Foundation
+import WrocliveTestsShared
+@testable import WrocliveFramework
+
+private let interval = NotificationTokenSendLimiter.sendInterval
+private let dateInitial = Date(timeIntervalSince1970: 0.0)
+private let dateWithinInterval = Date(timeInterval: interval - 1.0, since: dateInitial)
+private let dateAfterInterval = Date(timeInterval: interval + 1.0, since: dateInitial)
+
+private var dateMock = dateInitial
+
+private func getDateMock() -> Date {
+  return dateMock
+}
+
+final class NotificationTokenSendLimiterTests: XCTestCase {
+
+  // MARK: - Should send
+
+  func test_shouldSend_if_tokenWasNotStored() {
+    let store = NotificationTokenStoreMock()
+    let limiter = NotificationTokenSendLimiter(store: store, getDate: getDateMock)
+
+    XCTAssertNil(store.storedNotificationToken) // Not stored
+
+    dateMock = dateInitial
+    let result = limiter.shouldSend(token: "TOKEN_VALUE")
+
+    XCTAssertTrue(result)
+    XCTAssertEqual(store.getNotificationTokenCount, 1)
+    XCTAssertEqual(store.setNotificationTokenCount, 0)
+  }
+
+  func test_shouldSend_when_tokenChanged() {
+    let store = NotificationTokenStoreMock()
+    let limiter = NotificationTokenSendLimiter(store: store, getDate: getDateMock)
+
+    store.storedNotificationToken = StoredNotificationToken(
+      date: dateInitial,
+      value: "OLD_TOKEN_VALUE"
+    )
+
+    dateMock = dateWithinInterval
+    let result = limiter.shouldSend(token: "TOKEN_VALUE")
+
+    XCTAssertTrue(result)
+    XCTAssertEqual(store.getNotificationTokenCount, 1)
+    XCTAssertEqual(store.setNotificationTokenCount, 0)
+  }
+
+  func test_shouldSend_when_storedToken_isFromFuture() {
+    let store = NotificationTokenStoreMock()
+    let limiter = NotificationTokenSendLimiter(store: store, getDate: getDateMock)
+
+    store.storedNotificationToken = StoredNotificationToken(
+      date: dateWithinInterval,
+      value: "TOKEN_VALUE"
+    )
+
+    dateMock = dateInitial
+    let result = limiter.shouldSend(token: "TOKEN_VALUE")
+
+    XCTAssertTrue(result)
+    XCTAssertEqual(store.getNotificationTokenCount, 1)
+    XCTAssertEqual(store.setNotificationTokenCount, 0)
+  }
+
+  func test_shouldSend_after_sendInterval() {
+    let store = NotificationTokenStoreMock()
+    let limiter = NotificationTokenSendLimiter(store: store, getDate: getDateMock)
+
+    store.storedNotificationToken = StoredNotificationToken(
+      date: dateInitial,
+      value: "TOKEN_VALUE"
+    )
+
+    dateMock = dateAfterInterval
+    let result = limiter.shouldSend(token: "TOKEN_VALUE")
+
+    XCTAssertTrue(result)
+    XCTAssertEqual(store.getNotificationTokenCount, 1)
+    XCTAssertEqual(store.setNotificationTokenCount, 0)
+  }
+
+  func test_shouldNotSend_within_sendInterval() {
+    let store = NotificationTokenStoreMock()
+    let limiter = NotificationTokenSendLimiter(store: store, getDate: getDateMock)
+
+    store.storedNotificationToken = StoredNotificationToken(
+      date: dateInitial,
+      value: "TOKEN_VALUE"
+    )
+
+    dateMock = dateWithinInterval
+    let result = limiter.shouldSend(token: "TOKEN_VALUE")
+
+    XCTAssertFalse(result)
+    XCTAssertEqual(store.getNotificationTokenCount, 1)
+    XCTAssertEqual(store.setNotificationTokenCount, 0)
+  }
+
+  // MARK: - Register send
+
+  func test_registerSend_storesToken() {
+    let store = NotificationTokenStoreMock()
+    let limiter = NotificationTokenSendLimiter(store: store, getDate: getDateMock)
+
+    XCTAssertNil(store.storedNotificationToken)
+
+    dateMock = dateInitial
+    let token = "TOKEN_VALUE"
+    limiter.registerSend(token: token)
+
+    let expected = StoredNotificationToken(date: dateInitial, value: token)
+    XCTAssertEqual(store.storedNotificationToken, expected)
+    XCTAssertEqual(store.setNotificationTokenCount, 1)
+    XCTAssertEqual(store.getNotificationTokenCount, 0)
+  }
+}
